1e909daef57d55e471356775e5f793b66f8a7631
Arian <arian@MacBook-Pro-de-Arian.local>
2023-12-12 22:58:21 +0100

    example 2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { QueryClient, QueryClientProvider,  } from '@tanstack/react-query'
-import './App.css'
-import Container from './components/container/Container'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+
+import Container from './components/container/Container';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-  const queryClient = new QueryClient()
+	const queryClient = new QueryClient();
 
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Container />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  )
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Container />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
 }
 
-export default App
+export default App;
--- src/utils/v1/api.js
+++ src/utils/v1/api.js
@@ -1,36 +1,43 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "http://localhost:3001/",
 });
 
-export const getAllUsers = () => {
-  return api
-    .get("/user")
-    .then((res) => res.data)
-    .catch((e) => console.log(e));
+export const getAllUsers = async () => {
+  try {
+    const response = await api.get("user");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    throw error; // Rethrow the error for React Query to catch
+  }
 };
 
+
 export const createUser = (user) => {
+  console.log("Hola Entro aqui")
   return api
-    .post("/user", user)
+    .post("user", user)
     .then((res) => res.data)
     .catch((e) => console.log(e));
 };
 
 export const deleteUserById = (id) => {
+  console.log("Hola Entro aqui")
   return api
-    .delete(`/user/${id}`)
+    .delete(`user/${id}`)
     .then((res) => res.data)
     .catch((e) => console.log(e));
 };
 
 export const updateUserById = (data) => {
+  console.log(data)
   const { _id, name, email, password } = data;
   console.log(_id);
   const user = { name, email, password };
   return api
-    .patch(`/user/${_id}`, user)
+    .patch(`user/${_id}`, user)
     .then((res) => res.data)
     .catch((e) => console.log(e));
 };
